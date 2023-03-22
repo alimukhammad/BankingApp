d2196425c30fd1ca5f10ff8d13b3cba59aa47ab5
sardor <alimukhammad1@gmail.com>
2023-03-21 23:17:44 -0400

    Final change for bank login application

--- BankingApp2/BankingGUI.java
+++ BankingApp2/BankingGUI.java
@@ -1,59 +1,61 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class BankingGUI extends JFrame {
     private JTextField unField;
-    private JTextField psField;
+    private JPasswordField psField;
     
     public BankingGUI() {
         setTitle("Banking Application");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
+        setSize(300, 200);
         setLocationRelativeTo(null);
         
-        //menu bar
-        JMenuBar mb = new JMenuBar();
-
-        //file menu
-        JMenu mFile = new JMenu("File");
-        JMenuItem mOpen = new JMenuItem("Open");
-        JMenuItem mSaveAs = new JMenuItem("Save as");
-        mFile.add(mSaveAs);
-        mFile.add(mOpen);
-        mb.add(mFile);
-        //support menu
-        setJMenuBar(mb);
-        JMenu mHelp = new JMenu("Help");
-        JMenuItem mSupport = new JMenuItem("Support Link");
-        mHelp.add(mSupport);
-        mb.add(mHelp);
-
-        //username label & field
-        JLabel unLabel = new JLabel("Username");
+        // Create username label and field
+        JLabel unLabel = new JLabel("Username:");
         unField = new JTextField(20);
         JPanel unPanel = new JPanel(new FlowLayout());
         unPanel.add(unLabel);
         unPanel.add(unField);
-
-
-        //password label & field
-        JLabel psLabel = new JLabel("Password");
-        psField = new JTextField(20);
+        
+        // Create password label and field
+        JLabel psLabel = new JLabel("Password:");
+        psField = new JPasswordField(20);
         JPanel psPanel = new JPanel(new FlowLayout());
         psPanel.add(psLabel);
         psPanel.add(psField);
-
-
+        
+        // Create login button
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = unField.getText();
+                char[] passwordChars = psField.getPassword();
+                String password = new String(passwordChars);
+                
+                // Check login credentials
+                if (username.equals("admin") && password.equals("password")) {
+                    JOptionPane.showMessageDialog(BankingGUI.this, "Login successful!");
+                } else {
+                    JOptionPane.showMessageDialog(BankingGUI.this, "Login failed. Please try again.");
+                }
+                
+                // Clear password field
+                psField.setText("");
+            }
+        });
+        
         // Create panel for login button
         JPanel loginPanel = new JPanel(new FlowLayout());
         loginPanel.add(loginButton);
-
+        
+        // Add panels to frame
         JPanel mainPanel = new JPanel(new GridLayout(3, 1));
         mainPanel.add(unPanel);
         mainPanel.add(psPanel);
         mainPanel.add(loginPanel);
         add(mainPanel, BorderLayout.CENTER);
-
 
         setVisible(true);
     }

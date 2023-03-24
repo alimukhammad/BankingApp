3cb766bb96cf08fd7eb841730ab5c954ed0c4e96
sardor <alimukhammad1@gmail.com>
2023-03-24 00:23:56 -0400

    add on

--- ATM/Account.java
+++ ATM/Account.java
@@ -3,8 +3,30 @@ import java.util.ArrayList;
 public class Account {
 
     private String name;    //account name
-    private double balance; //account balance
     private String uuid;    //account ID
     private User holder;    //User object owner
     private ArrayList<Transaction> transaction; //transaction list
+
+    //Create a new user
+        // @param firstName    the user's first name
+        // @param lastName     the user's last name
+        // @param pin          the user's pin number
+        // @param theBank      the Bank object parent of customer
+
+    public Account(String name, User holder, Bank theBank){
+
+        //set the account name and holder
+        this.name = name;
+        this.holder = holder;
+
+        //get new account UUID
+        this.uuid = theBank.getNewAccountUUID();
+
+        //init transaction 
+        this.transaction = new ArrayList<Transaction>();
+
+        //add to holder and bank lists
+        holder.addAccount(this);
+        theBank.addAccount(this);
+    }
 }
--- ATM/User.java
+++ ATM/User.java
@@ -1,10 +1,56 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class User<Account> {
     
-    private String fname; //user name
-    private String lname; //user last name
+    private String firstname; //user name
+    private String lastname; //user last name
     private String uuid;  //user ID number
-    private byte pinHash; //user pin
+    private byte pinHash[]; //user pin
     private ArrayList<Account> account; // list of account for the user 
+
+    //Create a new user
+        // @param firstName    the user's first name
+        // @param lastName     the user's last name
+        // @param pin          the user's pin number
+        // @param theBank      the Bank object parent of customer
+
+    public User(String firstName, String lastName, String pin, Bank theBank){
+
+        //set user's name
+        this.firstname = firstName;
+        this.lastname = lastName;
+
+        //store the pin's MD5 hash, rather than the original value, for security reason
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            this.pinHash = md.digest(pin.getBytes());
+        }catch(NoSuchAlgorithmException e){
+            System.err.println("error, caught NoSuchAlgorithmException");
+            e.printStackTrace();
+            System.exit(1);
+        }    
+
+        //get a new, unique universal ID for the user
+        this.uuid = theBank.getNewUserUUID();
+
+        //create empty list of accounts
+        this.account = new ArrayList<Account>();
+
+        //print log message
+        System.out.printf("New user %s, %s with ID %s create.\n", lastName, firstName, this.uuid);
+
+    }
+
+    //Add an Account for the user
+        //@param anAcct the account to add
+    public void addAccount(Account anAcct){
+        this.account.add(anAcct);
+    
+    }
+
+    public String getUUID(){
+        return this.uuid;
+    }
 }
